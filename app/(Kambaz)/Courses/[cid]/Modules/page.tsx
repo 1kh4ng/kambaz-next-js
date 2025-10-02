c51e385600235de74689981d59d8b04c0a12991d
1kh4ng <khangp2004nj@gmail.com>
2025-10-02 16:45:26 -0400

    Minor fix lol

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -12,7 +12,7 @@ export default function CourseModules({ params }: { params: { cid: string } }) {
       </div>
 
       <div className="wd-module">
-        <h3 className="wd-module-title">Module 2 – Prototyping the React Kambaz User Interface with HTML<</h3>
+        <h3 className="wd-module-title">Module 2 – Prototyping the React Kambaz User Interface with HTML</h3>
         <ul className="wd-lessons">
           <li className="wd-lesson">Lesson 1 – Lesson 2.1</li>
           <li className="wd-lesson">Lesson 2 – Lesson 2.2</li>
