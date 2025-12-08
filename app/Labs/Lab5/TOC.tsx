7f7125662b674f5f1a83383838293a43cd207d32
1kh4ng <khangp2004nj@gmail.com>
2025-12-08 01:17:59 -0500

    ur annoying

--- app/Labs/Lab5/TOC.tsx
+++ app/Labs/Lab5/TOC.tsx
@@ -4,4 +4,24 @@ import Link from "next/link";
   <Link href="/Labs/Lab5" id="wd-lab5-link">
     Lab 5
   </Link>
+  <ul>
+    <li>
+      <a
+        href="https://github.com/1kh4ng/kambaz-node-server-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Node Server GitHub Repository
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://kambaz-node-server-app-k6dq.onrender.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Node Server on Render
+      </a>
+    </li>
+  </ul>
 </li>
